bc1ec55594e69b14d46a7bb97c37fc34886ff7a9
hangx-ma <m-contour@qq.com>
2024-01-08 12:07:26 +0800

    fix: update obstacle config

--- include/dino/obstacle.cpp
+++ include/dino/obstacle.cpp
@@ -12,7 +12,7 @@ bool Obstacle::updateCactus(lgfx::LGFX_Sprite *screen, RenderConfig_t &render_cf
 {
     if (cactus_.update(screen, render_cfg, cactus_type_, dino_alive)) {
         // Low game speed only use 2 cactus type: MIDDLE and SMALL
-        if (render_cfg.game_speed < 42) {
+        if (render_cfg.game_speed < 30) {
             cactus_type_ = static_cast<CactusType>(random_generator_.next(1, 2));
         }
         else {
